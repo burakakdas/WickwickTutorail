36f712e9321a392d4924e3607f87bf40b7d16ddb
burak <burak.akdas1@gmail.com>
2025-02-10 22:02:46 +0300

    v0.10 Spatula added

--- Assets/_GameAssets/Scripts/Gameplay/Player/PlayerİnteractionController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Player/PlayerİnteractionController.cs
@@ -2,11 +2,26 @@ using UnityEngine;
 
 public class PlayerİnteractionController : MonoBehaviour
 {
+    [SerializeField] PlayerController _playerComtroller;
+
+    private void Awake()
+    {
+        _playerComtroller = GetComponent<PlayerController>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.TryGetComponent<ICollectible>(out var collectible))
         {
             collectible.Collect();
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.TryGetComponent<IBoostable>(out var boostable))
+        {
+            boostable.Boost(_playerComtroller);
         }
     }
 }
