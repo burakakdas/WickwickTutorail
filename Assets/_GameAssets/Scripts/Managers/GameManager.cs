adff09c9491a31659fbfa81db7684335316f6638
burak <burak.akdas1@gmail.com>
2025-02-15 15:43:07 +0300

    v0.15 Popups added

--- Assets/_GameAssets/Scripts/Managers/GameManager.cs
+++ Assets/_GameAssets/Scripts/Managers/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
 
     [Header("References")]
     [SerializeField] private EggCounterUI _eggCounterUI;
+    [SerializeField] private WinLoseUI _winLoseUI;
 
     [Header("Settings")]
 
@@ -42,12 +43,10 @@ public class GameManager : MonoBehaviour
         
         if(_currentEggCount == _maxEggCount)
         {
-            //win
-            Debug.Log("game win");
             _eggCounterUI.SetEggCompleted();
             ChangeGameState(GameState.GameOver);
+            _winLoseUI.OnGameWin();
         }
-        Debug.Log("Egg Count: " + _currentEggCount);
     }
 
     public GameState GetCurrentGameState()
--- Assets/_GameAssets/Scripts/UI/TimerUI.cs
+++ Assets/_GameAssets/Scripts/UI/TimerUI.cs
@@ -16,6 +16,7 @@ public class TimerUI : MonoBehaviour
     private float _elapsedTime;
     private bool _isTimerRunning;
     private Tween _rotationTween;
+    private string _finalTime;
 
     private void Start()
     {
@@ -27,13 +28,16 @@ public class TimerUI : MonoBehaviour
 
     private void GameManager_OnGameStateChanged(GameState gameState)
     {
-        switch(gameState)
+        switch (gameState)
         {
             case GameState.Pause:
-                PauseTimer();
+                StopTimer();
                 break;
             case GameState.Resume:
                 ResumeTimer();
+                break;
+            case GameState.GameOver:
+                FinishTimer();
                 break;
         }
     }
@@ -52,7 +56,7 @@ public class TimerUI : MonoBehaviour
         InvokeRepeating(nameof(UpdateTimerUI), 0f, 1f);
     }
 
-    private void PauseTimer()
+    private void StopTimer()
     {
         _isTimerRunning = false;
         CancelInvoke(nameof(UpdateTimerUI));
@@ -61,7 +65,7 @@ public class TimerUI : MonoBehaviour
 
     private void ResumeTimer()
     {
-        if(!_isTimerRunning)
+        if (!_isTimerRunning)
         {
             _isTimerRunning = true;
             InvokeRepeating(nameof(UpdateTimerUI), 0f, 1f);
@@ -69,14 +73,33 @@ public class TimerUI : MonoBehaviour
         }
     }
 
+    private void FinishTimer()
+    {
+        StopTimer();
+        _finalTime = GetFormattedElapsedTime();
+    }
+
+    private string GetFormattedElapsedTime()
+    {
+        int minutes = Mathf.FloorToInt(_elapsedTime / 60f);
+        int seconds = Mathf.FloorToInt(_elapsedTime % 60f);
+
+        return string.Format("{00:00}:{1:00}", minutes, seconds);
+    }
+
     private void UpdateTimerUI()
     {
-        if(!_isTimerRunning) { return; }
+        if (!_isTimerRunning) { return; }
         _elapsedTime += 1f;
 
         int minutes = Mathf.FloorToInt(_elapsedTime / 60f);
         int seconds = Mathf.FloorToInt(_elapsedTime % 60f);
 
         _timerText.text = string.Format("{00:00}:{1:00}", minutes, seconds);
+    }
+
+    public string GetFinalTime()
+    {
+        return _finalTime;
     }
 }
